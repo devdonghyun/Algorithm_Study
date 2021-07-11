e6915112415ccd9457d12a0d80c95ac3a77d72f2
devDongHYun <ehdgus8725@naver.com>
2021-07-11 12:23:54 +0900

    Solve 1이 될 때까지

---
+++ greedy/book/1이 될 때까지.py
@@ -0,0 +1,14 @@
+def untilOne(n, k):
+    count = 0
+    while (n != 1):
+        if n % k == 0:
+            n /= k
+        else:
+            n -= 1
+        count += 1
+
+    return count
+
+
+n, k = map(int, input().split())
+print(untilOne(n, k))
