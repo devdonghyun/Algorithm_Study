7cb2e3181f7ea715c0b265ceb50599fd9566e543
devDongHYun <ehdgus8725@naver.com>
2021-07-11 12:31:04 +0900

    Study More About / & //

--- greedy/book/1이 될 때까지.py
+++ greedy/book/1이 될 때까지.py
@@ -1,8 +1,12 @@
+# greedy - 1이 될 때까지
+# /, // 주의해서 사용하기
+
 def untilOne(n, k):
     count = 0
     while (n != 1):
         if n % k == 0:
-            n /= k
+            # n /= k
+            n //= k  # n이 실수가 되는 일 방지
         else:
             n -= 1
         count += 1
